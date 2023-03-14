f9e3b7c54e6d6037c730adade009fb098349c8c2
Arsalan Sefidgar <arsalan.sefidgar@gmail.com>
2023-03-14 13:25:43 +0330

    add cnf

--- database-backup-storage/files/db_backup.sh
+++ database-backup-storage/files/db_backup.sh
@@ -11,7 +11,7 @@ BUCKET_SUBDIR=""
 
 mkdir -p $DEST
 
-nice mysqldump -h $DB_HOST --port $DB_PORT -u $DB_USER -p "mysql_password" --quick --ignore-table=telescope_entries --single-transaction --opt $DB_NAME | gzip > "$DEST"/dbbackup.gz
+nice mysqldump -h $DB_HOST --port $DB_PORT -u $DB_USER --defaults-extra-file=/etc/mysql/mysqlpassword.cnf --quick --ignore-table=$DB_NAME.telescope_entries --single-transaction --opt $DB_NAME | gzip > "$DEST"/dbbackup.gz
 
 ~/s3/mc cp $DEST/dbbackup.gz backup/g2-backup-dbs/$BUCKET_SUBDIR/dbbackup.gz
 
