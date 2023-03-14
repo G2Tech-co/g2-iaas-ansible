ff69c847f803ed84107f1c95d5870b3c5df5b5cd
Arsalan Sefidgar <arsalan.sefidgar@gmail.com>
2023-03-14 14:24:33 +0330

    fix pass

--- database-backup-storage/files/db_backup.sh
+++ database-backup-storage/files/db_backup.sh
@@ -3,15 +3,16 @@
 set -x
 
 DEST=~/db_backups
-DB_HOST="localhost"
-DB_PORT=33306
-DB_USER="root"
-DB_NAME="bms"
-BUCKET_SUBDIR=""
+DB_HOST="127.0.0.1"
+DB_PORT=3306
+DB_USER="user"
+DB_PASS="'pass'"
+DB_NAME="dbname"
+BUCKET_SUBDIR="sub"
 
 mkdir -p $DEST
 
-nice mysqldump -h $DB_HOST --port $DB_PORT -u $DB_USER --defaults-extra-file=/etc/mysql/mysqlpassword.cnf --quick --ignore-table=$DB_NAME.telescope_entries --single-transaction --opt $DB_NAME | gzip > "$DEST"/dbbackup.gz
+nice mysqldump -h $DB_HOST --port $DB_PORT -u $DB_USER -p$DB_PASS --ignore-table=$DB_NAME.telescope_entries --single-transaction --opt $DB_NAME | gzip > "$DEST"/dbbackup.gz
 
 ~/s3/mc cp $DEST/dbbackup.gz backup/g2-backup-dbs/$BUCKET_SUBDIR/dbbackup.gz
 
